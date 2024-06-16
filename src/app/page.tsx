ba370019aee0deaf0ea52762d5b848f53de76f5c
Adarsh <adarsh9540984202@gmail.com>
2024-06-16 18:26:43 +0530

    hero section updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import { LampDemo } from "./components/LampDemo";
+import { AuroraBackgroundDemo } from "./components/AuroraBackgroundDemo";
 
 
 export default function Home() {
   return (
     <div>
-     <LampDemo/>
+     {/* <LampDemo/> */}
+     <AuroraBackgroundDemo/>
     </div>
   );
 }
