cd78bb287af18de73471d21da7bc7641bff8b20d
Adarsh <adarsh9540984202@gmail.com>
2024-06-18 20:44:22 +0530

    all pages has been linked

--- src/app/components/AuroraBackgroundDemo.tsx
+++ src/app/components/AuroraBackgroundDemo.tsx
@@ -3,8 +3,14 @@
 import { motion } from "framer-motion";
 import React from "react";
 import { AuroraBackground } from "../components/ui/aurora-background";
-
+import { useRouter } from 'next/navigation'; 
 export function AuroraBackgroundDemo() {
+  const router = useRouter();
+
+  const handleSignUp = async () => {
+    router.push("./signup")
+}
+
   return (
     <AuroraBackground className="bg-black">
       <motion.div
@@ -23,7 +29,9 @@ export function AuroraBackgroundDemo() {
         <div className="font-extralight text-base md:text-4xl text-white py-4">
         Explore Every Topic!
         </div>
-        <button className="bg-white dark:bg-black rounded-full w-fit text-black dark:text-white px-4 py-2">
+        <button 
+        onClick={handleSignUp}
+        className="bg-white dark:bg-black rounded-full w-fit text-black dark:text-white px-4 py-2">
           Sign Up
         </button>
       </motion.div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
+"use client"
 import Image from "next/image";
 import { LampDemo } from "./components/LampDemo";
 import { AuroraBackgroundDemo } from "./components/AuroraBackgroundDemo";
-
 
 export default function Home() {
   return (
