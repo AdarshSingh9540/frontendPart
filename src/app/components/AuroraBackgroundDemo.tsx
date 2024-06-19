3406802b45ba83d671e03bbc49d43b96f5e223d6
Adarsh <adarsh9540984202@gmail.com>
2024-06-19 19:15:38 +0530

    okh

--- src/app/components/AuroraBackgroundDemo.tsx
+++ src/app/components/AuroraBackgroundDemo.tsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 import React from "react";
 import { AuroraBackground } from "../components/ui/aurora-background";
 import { useRouter } from 'next/navigation'; 
-import { Button } from "../components/ui/moving-border";
+
 
 
 export function AuroraBackgroundDemo() {
